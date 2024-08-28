113f04950b594db489f612b2ec83a086baec9c27
Gaurav kumar yadav <97657189+gauravyad12@users.noreply.github.com>
2024-08-29 04:21:58 +0530

    Update useChat.jsx

--- src/hooks/useChat.jsx
+++ src/hooks/useChat.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-const backendUrl = "http://localhost:3000";
+const backendUrl = "https://dark-backend.vercel.app/";
 
 const ChatContext = createContext();
 
